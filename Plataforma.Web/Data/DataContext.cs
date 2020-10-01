aa3549315936dc22288e8e7bcf70f5ca986fac11
Edwin Mauricio Gonzalez Moreno <egonzalez@gasolutions.com.co>
2020-10-01 07:03:53 -0500

    Conexion BD Local y creacion controlador Producto

---
+++ Plataforma.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Plataforma.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Plataforma.Web.Data.Entities;
+    using System;
+
+ 
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+              
+        }
+    }
+}
